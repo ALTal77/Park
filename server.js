c68131dbb6c58e774befd315f702eba49bfd9e48
Ahmadaltal <ahmadtal20200@gmail.com>
2024-11-26 23:09:30 +0300

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+    const express = require('express');
+    const path = require('path');
 
-const app = express();
+    const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
 
-app.listen(8081, () => {
-    console.log('Server is listening on http://localhost:8081');
-});
+    app.listen(8081, () => {
+        console.log('Server is listening on http://localhost:8081');
+    });
 
 
