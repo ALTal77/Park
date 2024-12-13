604d595aff1d42bcd287b329235ca5d928c6a9aa
Ahmadaltal <ahmadtal20200@gmail.com>
2024-12-13 11:02:00 +0300

    Reupload website

---
+++ server.js
@@ -0,0 +1,16 @@
+    const express = require('express');
+    const path = require('path');
+
+    const app = express();
+
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
+
+    app.listen(8081, () => {
+        console.log('Server is listening on http://localhost:8081');
+    });
+
+
